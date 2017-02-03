2c4d15a677d42324e50239ee39c1a4f99b3f8184
Sam Warnick <sam.t.warnick@gmail.com>
2017-02-02 19:42:15 -0700

    Finish the three nodes parts
    I think

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -133,6 +133,7 @@ def main():
     n2 = net.get_node('n2')
     n3 = net.get_node('n3')
     n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    n1.add_forwarding_entry(address=n3.get_address('n2'), link=n1.links[0])
     n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
     n2.add_forwarding_entry(address=n3.get_address('n2'), link=n2.links[1])
     n3.add_forwarding_entry(address=n2.get_address('n3'), link=n3.links[0])
@@ -140,14 +141,91 @@ def main():
 
     # setup app
     d = DelayHandler()
-    net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+    # net.nodes['n2'].add_protocol(protocol="delay", handler=d)
     net.nodes['n3'].add_protocol(protocol="delay", handler=d)
 
     # send 1MB
 
     for i in range(0, 8000000/8000):
-        # don't know how to get to n3
-        p = Packet(destination_address=n2.get_address('n1'), ident=i + 1, protocol='delay', length=8000)
+        p = Packet(destination_address=n3.get_address('n2'), ident=i + 1, protocol='delay', length=8000)
+        transmissionDelay = 8000 / n1.links[0].bandwidth
+        calculatedDelay = i * transmissionDelay
+        Sim.scheduler.add(delay=calculatedDelay, event=p, handler=n1.send_packet)
+
+    # Sim.set_debug("Node")
+
+    # run the simulation
+    Sim.scheduler.run()
+
+    ## Part 2
+
+    print("PART 2")
+
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('networks/three_nodes_2.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n3 = net.get_node('n3')
+    n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    n1.add_forwarding_entry(address=n3.get_address('n2'), link=n1.links[0])
+    n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+    n2.add_forwarding_entry(address=n3.get_address('n2'), link=n2.links[1])
+    n3.add_forwarding_entry(address=n2.get_address('n3'), link=n3.links[0])
+
+
+    # setup app
+    d = DelayHandler()
+    # net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+    net.nodes['n3'].add_protocol(protocol="delay", handler=d)
+
+    # send 1MB
+
+    for i in range(0, 8000000/8000):
+        p = Packet(destination_address=n3.get_address('n2'), ident=i + 1, protocol='delay', length=8000)
+        transmissionDelay = 8000 / n1.links[0].bandwidth
+        calculatedDelay = i * transmissionDelay
+        Sim.scheduler.add(delay=calculatedDelay, event=p, handler=n1.send_packet)
+
+    # Sim.set_debug("Node")
+
+    # run the simulation
+    Sim.scheduler.run()
+
+    ## Part 3
+
+    print("PART 3")
+
+    # parameters
+    Sim.scheduler.reset()
+
+    # setup network
+    net = Network('networks/three_nodes_3.txt')
+
+    # setup routes
+    n1 = net.get_node('n1')
+    n2 = net.get_node('n2')
+    n3 = net.get_node('n3')
+    n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+    n1.add_forwarding_entry(address=n3.get_address('n2'), link=n1.links[0])
+    n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+    n2.add_forwarding_entry(address=n3.get_address('n2'), link=n2.links[1])
+    n3.add_forwarding_entry(address=n2.get_address('n3'), link=n3.links[0])
+
+
+    # setup app
+    d = DelayHandler()
+    # net.nodes['n2'].add_protocol(protocol="delay", handler=d)
+    net.nodes['n3'].add_protocol(protocol="delay", handler=d)
+
+    # send 1MB
+
+    for i in range(0, 8000000/8000):
+        p = Packet(destination_address=n3.get_address('n2'), ident=i + 1, protocol='delay', length=8000)
         transmissionDelay = 8000 / n1.links[0].bandwidth
         calculatedDelay = i * transmissionDelay
         Sim.scheduler.add(delay=calculatedDelay, event=p, handler=n1.send_packet)
