923a2f133356beaa0c33cf9f00c3a663bddff833
Sam Warnick <sam.t.warnick@gmail.com>
2017-03-01 01:25:40 -0700

    Add report

--- lab2/mytests.py
+++ lab2/mytests.py
@@ -42,18 +42,27 @@ def main():
     file_size = os.path.getsize("internet-architecture.pdf")
 
     data = []
-    time = tester.run(1000, 0, "internet-architecture.pdf", False)
-    data.append((1000, file_size/time))
-    time = tester.run(2000, 0, "internet-architecture.pdf", False)
-    data.append((2000, file_size/time))
-    time = tester.run(5000, 0, "internet-architecture.pdf", False)
-    data.append((5000, file_size/time))
-    time = tester.run(10000, 0, "internet-architecture.pdf", False)
-    data.append((10000, file_size/time))
-    time = tester.run(15000, 0, "internet-architecture.pdf", False)
-    data.append((15000, file_size/time))
-    time = tester.run(20000, 0, "internet-architecture.pdf", False)
-    data.append((20000, file_size/time))
+    time1 = tester.run(1000, 0, "internet-architecture.pdf", False)
+    data.append((1000, file_size/time1))
+    time2 = tester.run(2000, 0, "internet-architecture.pdf", False)
+    data.append((2000, file_size/time2))
+    time3 = tester.run(5000, 0, "internet-architecture.pdf", False)
+    data.append((5000, file_size/time3))
+    time4 = tester.run(10000, 0, "internet-architecture.pdf", False)
+    data.append((10000, file_size/time4))
+    time5 = tester.run(15000, 0, "internet-architecture.pdf", False)
+    data.append((15000, file_size/time5))
+    time6 = tester.run(20000, 0, "internet-architecture.pdf", False)
+    data.append((20000, file_size/time6))
+
+    segments = file_size / 1000
+    data2 = []
+    data2.append((1000, time1 - time6 / segments))
+    data2.append((2000, time2 - time6 / segments))
+    data2.append((5000, time3 - time6 / segments))
+    data2.append((10000, time4 - time6 / segments))
+    data2.append((15000, time5 - time6 / segments))
+    data2.append((20000, time6 - time6 / segments))
 
     df = pd.DataFrame(data = data, columns=['Window Size', 'Throughput']).sort_values(by=['Window Size', 'Throughput'], ascending=[True, True])
     plt.figure()
@@ -65,6 +74,15 @@ def main():
     fig = ax.get_figure()
     fig.savefig('window_size.png')
 
+    df = pd.DataFrame(data = data, columns=['Window Size', 'Queueing Delay']).sort_values(by=['Window Size', 'Queueing Delay'], ascending=[True, True])
+    plt.figure()
+
+    ax = df.plot(x="Queueing Delay",y="Window Size", color="blue")
+
+    ax.set_ylabel("Queueing Delay")
+    ax.set_xlabel("Window Size")
+    fig = ax.get_figure()
+    fig.savefig('queueing_delay.png')
 
 
 
