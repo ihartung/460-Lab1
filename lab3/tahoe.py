971e9475f5d9e04857da4318d6839ba4962ed063
Isaac Hartung <isaac.hartung@gmail.com>
2017-03-20 02:50:19 -0600

    rough finish of tcp_tahoe.py

---
+++ lab3/tahoe.py
@@ -0,0 +1,104 @@
+
+from __future__ import print_function
+
+import sys
+
+sys.path.append('..')
+
+from src.sim import Sim
+from src.transport import Transport
+from src.tcp import TCP
+
+from networks.network import Network
+
+import optparse
+import os
+import subprocess
+
+
+class AppHandler(object):
+    def __init__(self, filename):
+        self.filename = filename
+        self.directory = 'received'
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+        self.f = open(os.path.join(self.directory, self.filename), 'wb')
+
+    def receive_data(self, data):
+        Sim.trace('AppHandler', "application got %d bytes" % (len(data)))
+        self.f.write(data)
+        self.f.flush()
+
+
+class Trans(object):
+    def __init__(self):
+        self.directory = 'received'
+
+    def diff(self):
+        args = ['diff', '-u', self.filename, os.path.join(self.directory, self.filename)]
+        result = subprocess.Popen(args, stdout=subprocess.PIPE).communicate()[0]
+        print()
+        if not result:
+            print("File transfer correct!")
+        else:
+            print("File transfer failed. Here is the diff:")
+            print()
+            print(result)
+
+    def run(self, janela, perda, fila, fast):
+        
+        self.filename = fila
+        loss     = perda
+        Sim.trace('Trans', "It is %s that I am using fast transmit." % (fast))
+        
+        # parameters
+        Sim.scheduler.reset()
+        Sim.set_debug('AppHandler')
+        Sim.set_debug('TCP')
+        Sim.set_debug('Trans')
+
+        # setup network
+        net = Network('./network.txt')
+        net.loss(loss)
+        
+
+        # setup routes
+        n1 = net.get_node('n1')
+        n2 = net.get_node('n2')
+        net.set_queue(n1.links[0], "100")
+        net.set_queue(n2.links[0], "100")
+        
+        n1.add_forwarding_entry(address=n2.get_address('n1'), link=n1.links[0])
+        n2.add_forwarding_entry(address=n1.get_address('n2'), link=n2.links[0])
+
+        # setup transport
+        t1 = Transport(n1)
+        t2 = Transport(n2)
+
+        # setup application
+        a = AppHandler(self.filename)
+
+        # setup connection
+        c1 = TCP(t1, n1.get_address('n2'), 1, n2.get_address('n1'), 1, a, janela, fast)
+        c2 = TCP(t2, n2.get_address('n1'), 1, n1.get_address('n2'), 1, a, janela, fast)
+
+        # send a file
+        with open(self.filename, 'rb') as f:
+            while True:
+                data = f.read(1000)
+                if not data:
+                    break
+                Sim.scheduler.add(delay=0, event=data, handler=c1.send)
+
+        # run the simulation
+        Sim.scheduler.run()
+
+        self.diff()
+
+        self.filename = None
+
+        return Sim.scheduler.current_time()
+
+
+if __name__ == '__main__':
+    m = Main()
---
+++ src/tcp_tahoe.py
@@ -0,0 +1,201 @@
+from .buffer import SendBuffer, ReceiveBuffer
+from .connection import Connection
+from .sim import Sim
+from .tcppacket import TCPPacket
+class TCP(Connection):
+    """ A TCP connection between two hosts."""
+
+    def __init__(self, transport, source_address, source_port,
+                 destination_address, destination_port, app=None, window=1000,drop=[]):
+                            destination_address, destination_port, app)
+        Connection.__init__(self, transport, source_address, source_port,
+
+        # -- Sender functionality
+
+        # maximum segment size, in bytes
+        self.mss = 1000 
+        # send window; represents the total number of bytes that may
+        # be outstanding at one time
+        self.window = self.mss 
+        # send buffer
+        self.send_buffer = SendBuffer()
+        # largest sequence number that has been ACKed so far; represents
+        # the next sequence number the client expects to receive
+        self.sequence = 0
+        # plot sequence numbers
+        self.plot_sequence_header()
+        # packets to drop
+        self.drop = drop
+        self.dropped = []
+        self.timer = None
+        # retransmission timer
+        # timeout duration in seconds
+        self.timeout = 1
+        self.threshold = 100000
+        self.additiveIncrease = False
+        self.increment = 0
+
+        # -- Receiver functionality
+
+        # receive buffer
+        self.receive_buffer = ReceiveBuffer()
+        # ack number to send; represents the largest in-order sequence
+        # number not yet received
+        self.ack = 0
+
+    def trace(self, message):
+        """ Print debugging messages. """
+        Sim.trace("TCP", message)
+
+    def plot_sequence_header(self):
+        if self.node.hostname =='n1':
+            Sim.plot('sequence.csv','Time,Sequence Number,Event\n')
+    def plot_sequence(self,sequence,event):
+
+            Sim.plot('sequence.csv','%s,%s,%s\n' % (Sim.scheduler.current_time(),sequence,event))
+        if self.node.hostname =='n1':
+
+    def receive_packet(self, packet):
+        """ Receive a packet from the network layer. """
+        if packet.ack_number > 0:
+            # handle ACK
+            self.handle_ack(packet)
+        if packet.length > 0:
+            # handle data
+            self.handle_data(packet)
+
+    ''' Sender '''
+
+    def send(self, data):
+
+        self.send_buffer.put(data)
+
+        if not self.timer:
+        # set a timer
+            self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+
+        while (self.send_buffer.outstanding() < self.window):
+            size = self.mss
+            d, s = self.send_buffer.get(size);
+            self.send_packet(d, s)
+            if not self.send_buffer.available():
+                return
+
+    def send_packet(self, data, sequence):
+        packet = TCPPacket(source_address=self.source_address,
+                           source_port=self.source_port,
+                           destination_address=self.destination_address,
+                           destination_port=self.destination_port,
+                           body=data,
+                           sequence=sequence, ack_number=self.ack)
+        if sequence in self.drop and not sequence in self.dropped:
+            self.dropped.append(sequence)
+            self.plot_sequence(sequence,'drop')
+            self.trace("%s (%d) dropping TCP segment to %d for %d" % (
+                self.node.hostname, self.source_address, self.destination_address, packet.sequence))
+            return
+
+        # send the packet
+        self.plot_sequence(sequence,'send')
+        self.trace("%s (%d) sending TCP segment to %d for %d" % (
+            self.node.hostname, self.source_address, self.destination_address, packet.sequence))
+        self.transport.send_packet(packet)
+
+        # set a timer
+        if not self.timer:
+            self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+
+    def handle_ack(self, packet):
+        """ Handle an incoming ACK. """
+        self.plot_sequence(packet.ack_number - 1000,'ack')
+        self.trace("%s (%d) received ACK from %d for %d" % (
+            self.node.hostname, packet.destination_address, packet.source_address, packet.ack_number))
+        self.cancel_timer()
+        if self.sequence >= packet.ack_number:
+            return
+
+        # Fast Restransmit
+        if packet.ack_number == self.sequence:
+            self.repeat = self.repeat + 1
+        else:
+            self.repeat   = 1
+            self.sequence = packet.ack_number
+            bytesReceived = packet.ack_number - self.sequence
+            if self.additiveIncrease:
+                # Once cwnd is larger than the threshold, use additive increase. Every time the sender receives an ACK for new data, increment cwnd by MSS*b/cwnd, where MSS is the maximum segment size (1000 bytes) and b is the number of new bytes acknowledged.
+                self.increment += (self.mss * bytesReceived / self.window)
+                if self.increment > self.mss:
+                    self.window += self.increment/self.mss * self.mss
+                    self.increment -= self.increment/self.mss * self.mss
+	    else:
+                # Every time the sender receives an ACK for new data, increment cwnd by the number of new bytes of data acknowledged.Never increment cwnd by more than one MSS.
+                #self.window += min(bytesReceived, self.mss)
+		self.window += bytesReceived
+                if self.window > self.threshold:
+                    # Stop slow start when cwnd exceeds or equals the threshold
+                    self.additiveIncrease = True
+
+
+            self.send_buffer.slide(packet.ack_number)
+
+        if self.repeat == 4:
+            # A loss event is detected when there are three duplicate ACKs (meaning the fourth ACK in a row for the same sequence number), and TCP immediately retransmits instead of waiting for the retransmission timer.
+            self.trace("fast_restransmit.  seq = %d" % (self.sequence))
+            self.retransmit()
+	    return
+
+        # self.sequence = packet.ack_number
+        # self.send_buffer.slide(packet.ack_number)
+        self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+        while (self.send_buffer.outstanding() < self.window):
+            size = self.mss
+            d, s = self.send_buffer.get(size);
+            self.send_packet(d, s)
+            if not self.send_buffer.available():
+                return
+        if self.send_buffer.outstanding() == 0 and self.send_buffer.available() == 0:
+            return
+        size = self.mss
+    def retransmit(self, event):
+        # When a loss event is detected (a timeout or 3 duplicate ACKs), then set the threshold to max(cwnd/2,MSS) and set cwnd to 1 MSS.
+        halfCWND = self.window/2
+        halfCWND = (halfCWND-(halfCWND % self.mss))
+        self.threshold = max(halfCWND, self.mss)
+        self.window = self.mss
+        self.increment = 0
+	self.additiveIncrease = False
+
+        d, s = self.send_buffer.resend(size)
+        self.send_packet(d, s)
+        self.timer = Sim.scheduler.add(delay=self.timeout, event='retransmit', handler=self.retransmit)
+    def cancel_timer(self):
+        """ Cancel the timer. """
+        if not self.timer:
+            return
+        Sim.scheduler.cancel(self.timer)
+        self.timer = None
+    ''' Receiver '''
+
+    def handle_data(self, packet):
+        """ Handle incoming data. This code currently gives all data to
+            the application, regardless of whether it is in order, and sends
+        self.trace("%s (%d) received TCP segment from %d for %d" % (
+            an ACK."""
+        self.receive_buffer.put(packet.body, packet.sequence)
+            self.node.hostname, packet.destination_address, packet.source_address, packet.sequence))
+        d, s = self.receive_buffer.get()
+        self.app.receive_data(d)
+        self.ack = len(d) + s
+        self.trace("Sending this ack from handle data: %d" % (self.ack))
+        self.send_ack()
+    def send_ack(self):
+        """ Send an ack. """
+        packet = TCPPacket(source_address=self.source_address,
+                           source_port=self.source_port,
+                           destination_address=self.destination_address,
+                           sequence=self.sequence, ack_number=self.ack)
+                           destination_port=self.destination_port,
+        # send the packet
+        self.trace("%s (%d) sending TCP ACK to %d for %d" % (
+            self.node.hostname, self.source_address, self.destination_address, packet.ack_number))
+        self.transport.send_packet(packet)
