8503e0aa7b7b9c97af78c82f98f3eabdf5854cb8
Isaac Hartung <isaac.hartung@gmail.com>
2017-04-19 12:45:43 -0600

    whatever

--- lab4/dvrouting.py
+++ lab4/dvrouting.py
@@ -3,6 +3,7 @@
 class DvroutingApp(object):
     def __init__(self, node):
         self.node = node
+        self.dv   = {}
 
     def receive_packet(self, packet):
         print(Sim.scheduler.current_time(), self.node.hostname, packet.ident)
